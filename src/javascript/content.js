f1ed4a8d0d172a9349008afc039f9395f156b70e
Luka Glusica <luka.glusica89@gmail.com>
2016-07-11 00:30:04 +0200

    Fix page responsivity

--- src/javascript/content.js
+++ src/javascript/content.js
@@ -2,45 +2,11 @@ $(document).ready(function () {
     /**
      * INITIAL
      */
-
-    if($(window).height() > 654){
-        var height = $(window).height() - 154;
-        var preview = 350 * height/500;
-        var code = 140 * height / 500;
-        $('#tools').css({
-            'min-height': height.toString() + 'px'
-        });
-
-        $('#preview').css ({
-            'min-height': preview.toString() + 'px'
-        });
-
-        $('#code').css ({
-            'min-height': code.toString() + 'px',
-            'margin-top': (height - preview - code).toString() + 'px'
-        });
-        $('#server_data').css({
-            'min-height': height.toString() + 'px'
-        });
-
-    } else {
-        $('#tools').css({
-            'min-height': '500px'
-        });
-
-        $('#preview').css ({
-            'min-height': '350px'
-        });
-
-        $('#code').css ({
-            'min-height': '140px',
-            'margin-top': '10px'
-        });
-
-        $('#server_data').css({
-            'min-height': '500px'
-        });
-    }
+    var titlediv = $('#title').height();
+    var title_p = $('#title_p').height();
+    $('#title_p').css({
+        'margin-top': (titlediv/2 - title_p/2).toString() + 'px'
+    });
 
     if($(window).width() < 753) {
         $('#categories_list').css({
@@ -54,47 +20,143 @@ $(document).ready(function () {
     $('#menu').click(function () {
         $('#categories_list').slideToggle();
     });
+    var contentHeight = $('#content').height();
+
+    $('#tools').css({
+        'min-height': contentHeight.toString() + 'px'
+    });
+
+    $('#output').css({
+        'min-height': contentHeight.toString() + 'px'
+    });
+
+    $('#server_data').css({
+        'min-height': contentHeight.toString() + 'px'
+    });
+    var previewHeight = contentHeight * 70 / 100;
+    var codeHeight = contentHeight * 28 / 100;
+    $('#preview').css({
+        'min-height': previewHeight.toString() + 'px'
+    });
+
+    $('#code').css({
+        'min-height': codeHeight.toString() + 'px'
+    });
+
+    $('#code').css({
+        'margin-top': ($('#content').height() - 40 - $('#preview').height() - $('#code').height()).toString() + 'px'
+    });
+
+    var diff = $(window).height() - 154 - contentHeight;
+    console.log(diff);
+    var t = $('#tools').height();
+    var o = $('#output').height();
+    var s = $('#server_data').height();
+    var check = [t, o, s];
+    var max = Math.max.apply(Math, check);
+    if(diff >= 0) {
+        if(max >= contentHeight) {
+            max += diff;
+            console.log('max > content');
+            $('#content').css({
+                'min-height': max.toString() + 'px'
+            });
+
+            $('#tools').css({
+                'min-height': max.toString() + 'px'
+            });
+
+            $('#output').css({
+                'min-height': max.toString() + 'px'
+            });
+
+            $('#server_data').css({
+                'min-height': max.toString() + 'px'
+            });
+            var previewHeight = (contentHeight + diff) * 70 / 100;
+            var codeHeight = (contentHeight + diff) * 28 / 100;
+            $('#preview').css({
+                'min-height': previewHeight.toString() + 'px'
+            });
+
+            $('#code').css({
+                'min-height': codeHeight.toString() + 'px'
+            });
+
+            $('#code').css({
+                'margin-top': ($('#content').height() - 40 - $('#preview').height() - $('#code').height()).toString() + 'px'
+            });
+        }
+    } else {
+        if(max >= contentHeight){
+            $('#content').css({
+                'min-height': max.toString() + 'px'
+            });
+
+            $('#tools').css({
+                'min-height': max.toString() + 'px'
+            });
+            $('#output').css({
+                'min-height': max.toString() + 'px'
+            });
+            $('#server_data').css({
+                'min-height': max.toString() + 'px'
+            });
+            var previewHeight = contentHeight * 70 / 100;
+            var codeHeight = contentHeight * 28 / 100;
+            $('#preview').css({
+                'min-height': previewHeight.toString() + 'px'
+            });
+
+            $('#code').css({
+                'min-height': codeHeight.toString() + 'px'
+            });
+
+            $('#code').css({
+                'margin-top': ($('#content').height() - 44 - $('#preview').height() - $('#code').height()).toString() + 'px'
+            });
+        }
+
+    }
+    if ($(window).width() < 992) {
+        $('#content').css({
+            'min-height': ''
+        });
+
+        $('#tools').css({
+            'min-height': '',
+            'padding': '10px 0 10px 0'
+        });
+        $('#output').css({
+            'min-height': ''
+        });
+        $('#server_data').css({
+            'min-height': ''
+        });
+        $('#preview').css({
+            'min-height': ''
+        });
+
+        $('#code').css({
+            'min-height': ''
+        });
+
+        $('#code').css({
+            'margin-top': '10px'
+        });
+    }
+
+
     /**
      * RESIZE
      */
     $(window).resize(function () {
-        if($(window).height() > 654){
-            var height = $(window).height() - 154;
-            var preview = 350 * height/500;
-            var code = 140 * height / 500;
-            $('#tools').css({
-                'min-height': height.toString() + 'px'
-            });
+        var titlediv = $('#title').height();
+        var title_p = $('#title_p').height();
+        $('#title_p').css({
+            'margin-top': (titlediv/2 - title_p/2).toString() + 'px'
+        });
 
-            $('#preview').css ({
-                'min-height': preview.toString() + 'px'
-            });
-
-            $('#code').css ({
-                'min-height': code.toString() + 'px',
-                'margin-top': (height - preview - code).toString() + 'px'
-            });
-            $('#server_data').css({
-                'min-height': height.toString() + 'px'
-            });
-        } else {
-            $('#tools').css({
-                'min-height': '500px'
-            });
-
-            $('#preview').css ({
-                'min-height': '350px'
-            });
-
-            $('#code').css ({
-                'min-height': '140px',
-                'margin-top': '10px'
-            });
-
-            $('#server_data').css({
-                'min-height': '500px'
-            });
-        }
         /**
          * MENU BAR and CATEGORIES
          */
@@ -109,4 +171,39 @@ $(document).ready(function () {
         }
 
     });
+
+    /*
+    * CATEGORIES&TOOLS
+    */
+
+    $('#creation').click(function () {
+        $('#tools_container>p').hide();
+        $('#tools_container>hr').hide();
+        $('#tools_container .tools_list').hide();
+        $('#tools_container>a').hide();
+        $('#tools_container>br').hide();
+        $('#creation_tools').fadeIn('fast');
+
+    });
+
+    $('#modification').click(function () {
+        $('#tools_container>p').hide();
+        $('#tools_container>hr').hide();
+        $('#tools_container .tools_list').hide();
+        $('#tools_container>a').hide();
+        $('#tools_container>br').hide();
+        $('#modification_tools').fadeIn('fast');
+
+    });
+
+    $('#deletion').click(function () {
+        $('#tools_container>p').hide();
+        $('#tools_container>hr').hide();
+        $('#tools_container .tools_list').hide();
+        $('#tools_container>a').hide();
+        $('#tools_container>br').hide();
+        $('#deletion_tools').fadeIn('fast');
+
+    });
+
 });
