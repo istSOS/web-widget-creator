ccdbbe34d8125e96f652a56e434d82a0a9746e50
Luka Glusica <luka.glusica89@gmail.com>
2017-07-17 22:42:14 +0200

    Add Main component

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,14 +1,30 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import Header from 'header';
+import Content from 'content';
+import Footer from 'footer';
 
 class Main extends Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			active: "home"
+		}
+
+		//this binding
+		this.changeTab = this.changeTab.bind(this);
+	}
+
+	changeTab(tab) {
+		this.setState({active: tab});
 	}
 
 	render() {
 		return (
 			<div className="main">
+				<Header activeTab={this.state.active} changeTab={this.changeTab}/>
+				<Content />
+				<Footer />
 			</div>
 		);
 	}
