d68bf9fb0bdeee44a7df4bcc04907ebe4763de9d
Luka Glusica <luka.glusica89@gmail.com>
2017-07-17 22:41:45 +0200

    Add Intro component

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,21 +1,17 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 
-class Intro extends Component {
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		return (
-			<div className="text-center intro">
+const Intro = (props) => {
+	return (
+		<div className="text-center intro">
+			<div className="intro-container">
 				<h1 className="intro-heading">IstSOS Web Widget Creator</h1>
-				<button className="btn btn-default intro-button" onClick={this.props.handleIntro}>
+				<button className="btn btn-default intro-button" onClick={props.handleIntro}>
 					<Link to="/home">Enter</Link>
 				</button>
 			</div>
-		);
-	}
+		</div>
+	);
 }
 
 export default Intro;
