6a48de4eeb7e161e7586783fd5a231f2cc302dac
Luka Glusica <luka.glusica89@gmail.com>
2017-07-23 02:35:25 +0200

    Modify dev configuration

--- webpack.config.js
+++ webpack.config.js
@@ -36,12 +36,24 @@ module.exports = {
       'content': path.resolve(__dirname, 'src/components/main/content/Content.jsx'),
       'footer': path.resolve(__dirname, 'src/components/main/Footer.jsx'),
       'home': path.resolve(__dirname, 'src/components/main/content/Home.jsx'),
+      'tool': path.resolve(__dirname, 'src/components/main/content/Tool.jsx'),
+      'sidebar-map': path.resolve(__dirname, 'src/components/main/content/sidebar/SidebarMap.jsx'),
+      'sidebar-box': path.resolve(__dirname, 'src/components/main/content/sidebar/SidebarBox.jsx'),
+      'sidebar-chart': path.resolve(__dirname, 'src/components/main/content/sidebar/SidebarChart.jsx'),
+      'display': path.resolve(__dirname, 'src/components/main/content/display/Display.jsx'),
+      'preview': path.resolve(__dirname, 'src/components/main/content/display/preview/Preview.jsx'),
+      'code': path.resolve(__dirname, 'src/components/main/content/display/code/Code.jsx'),
     },
     extensions: [".js", ".jsx", ".json"]
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin() // Enable HMR
+  ],
   devServer: {
     historyApiFallback: true,
     contentBase: './',
-    port: 9001
+    port: 9001,
+    inline: true,
+    hot: true
   }
 };
