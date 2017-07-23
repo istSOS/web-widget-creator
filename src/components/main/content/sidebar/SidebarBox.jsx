dad94579d5ffa24f2332f72139f6d57f9ecc1a0b
Luka Glusica <luka.glusica89@gmail.com>
2017-07-23 10:31:51 +0200

    Add SidebarChart component

--- src/components/main/content/sidebar/SidebarBox.jsx
+++ src/components/main/content/sidebar/SidebarBox.jsx
@@ -7,7 +7,123 @@ class SidebarBox extends Component {
 
 	render() {
 		return (
-			<div></div>
+			<div className="map text-center">
+				<div className="row text-center">
+					<div className="col-xs-12">
+					<h4>SENSOR SETTINGS</h4>
+					<table className="table sidebar-table">
+						<tbody>
+						<tr>
+							<td className="text-right">SERVICE:</td>
+							<td>
+								<select className="form-control">
+									<option>-- Select service from the list --</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td className="text-right">OFFERING:</td>
+							<td>
+								<select className="form-control">
+									<option>-- Select offering from the list --</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td className="text-right">PROCEDURE:</td>
+							<td>
+								<select className="form-control">
+									<option>-- Select observed property from the list --</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td className="text-right">PROPERTIES:</td>
+							<td></td>
+						</tr>
+						</tbody>
+					</table>
+					</div>
+				</div>
+				<hr/>
+				<div className="row text-center">
+					<div className="col-xs-12">
+						<h4>WIDGET SETTINGS</h4>
+						<span>LAYOUT</span><br/>
+						<label className="radio-inline"><input type="radio" name="optradio"/>HORIZONTAL</label>
+						<label className="radio-inline"><input type="radio" name="optradio"/>VERTICAL</label>
+						<table className="table sidebar-table">
+							<tbody>
+								<tr>
+									<td className="text-right">ID:</td>
+									<td>
+										<input type="text" className="form-control"/>
+									</td>
+								</tr>
+								<tr>
+									<td className="text-right">CLASS:</td>
+									<td>
+										<input type="text" className="form-control"/>
+									</td>
+								</tr>
+								<tr>
+									<td className="text-right">HEIGHT:</td>
+									<td>
+										<input type="text" className="form-control"/>
+									</td>
+								</tr>
+								<tr>
+									<td className="text-right">WIDTH:</td>
+									<td>
+										<input type="text" className="form-control"/>
+									</td>
+								</tr>
+								</tbody>
+						</table>
+					</div>
+				</div>
+				<hr/>
+				<div className="row text-center">
+					<div className="col-xs-12">
+						<h4>AUTOMATIC UPDATE SETTINGS</h4>
+						<div className="checkbox">
+  							<label><input type="checkbox" value=""/>ON/OFF</label>
+						</div>
+						<table className="table sidebar-table">
+							<tbody>
+								<tr>
+									<td className="text-right">TIME UNIT:</td>
+									<td>
+										<select className="form-control">
+											<option>Seconds</option>
+											<option>Minutes</option>
+											<option>Hours</option>
+										</select>
+									</td>
+								</tr>
+								<tr>
+									<td className="text-right">START DELAY:</td>
+									<td>
+										<input type="text" className="form-control"/>
+									</td>
+								</tr>
+								<tr>
+									<td className="text-right">INTERVAL:</td>
+									<td>
+										<input type="text" className="form-control"/>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<hr/>
+				<div className="row text-center home-end">
+					<div className="col-xs-12">
+						<button className="btn btn-danger">GENERATE</button>
+					</div>
+				</div>
+			</div>
 		)
 	}
 }
--- src/components/main/content/sidebar/SidebarChart.jsx
+++ src/components/main/content/sidebar/SidebarChart.jsx
@@ -7,7 +7,158 @@ class SidebarChart extends Component {
 
 	render() {
 		return (
-			<div></div>
+			(
+			<div className="map text-center">
+				<div className="row text-center">
+					<div className="col-xs-12">
+					<h4>SENSOR SETTINGS</h4>
+					<table className="table sidebar-table">
+						<tbody>
+						<tr>
+							<td className="text-right">SERVICE:</td>
+							<td>
+								<select className="form-control">
+									<option>-- Select service from the list --</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td className="text-right">OFFERING:</td>
+							<td>
+								<select className="form-control">
+									<option>-- Select offering from the list --</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td className="text-right">PROCEDURES:</td>
+							<td></td>
+						</tr>
+						<tr>
+							<td className="text-right">PROPERTIES:</td>
+							<td></td>
+						</tr>
+						</tbody>
+					</table>
+					</div>
+				</div>
+				<hr/>
+				<div className="row text-center">
+					<div className="col-xs-12">
+						<h4>TIME SPAN:</h4>
+						<table className="table sidebar-table">
+							<tbody>
+								<tr>
+									<td className="text-right">FROM:</td>
+									<td>
+										<input type="text" className="form-control"/>
+									</td>
+								</tr>
+								<tr>
+									<td className="text-right">TO:</td>
+									<td>
+										<input type="text" className="form-control"/>
+									</td>
+								</tr>
+								<tr>
+									<td className="text-right">GMT:</td>
+									<td>
+										<select className="form-control">
+											<option>-- Select time zone offset --</option>
+										</select>
+									</td>
+								</tr>
+								</tbody>
+						</table>
+					</div>
+				</div>
+				<hr/>
+				<div className="row text-center">
+					<div className="col-xs-12">
+						<h4>CHART SETTINGS:</h4>
+						<table className="table sidebar-table">
+							<tbody>
+								<tr>
+									<td className="text-right">TYPE:</td>
+									<td>
+										<select className="form-control">
+											<option>Line</option>
+											<option>Overview detail</option>
+											<option>Multivariable</option>
+											<option>Histogram</option>
+										</select>
+									</td>
+								</tr>
+								<tr>
+									<td className="text-right">AGGREGATE INTERVAL:</td>
+									<td>
+										<input type="text" className="form-control"/>
+									</td>
+								</tr>
+								<tr>
+									<td className="text-right">AGGREGATE FUNCTION:</td>
+									<td>
+										<input type="text" className="form-control"/>
+									</td>
+								</tr>
+								<tr>
+									<td className="text-right">COLOR:</td>
+									<td>
+										<input type="text" className="form-control"/>
+									</td>
+								</tr>
+								<tr>
+									<td className="text-right">COLOR 2:</td>
+									<td>
+										<input type="text" className="form-control"/>
+									</td>
+								</tr>
+								</tbody>
+						</table>
+					</div>
+				</div>
+				<hr/>
+				<div className="row text-center">
+					<div className="col-xs-12">
+						<h4>WIDGET SETTINGS</h4>
+						<table className="table sidebar-table">
+							<tbody>
+								<tr>
+									<td className="text-right">ID:</td>
+									<td>
+										<input type="text" className="form-control"/>
+									</td>
+								</tr>
+								<tr>
+									<td className="text-right">CLASS:</td>
+									<td>
+										<input type="text" className="form-control"/>
+									</td>
+								</tr>
+								<tr>
+									<td className="text-right">HEIGHT:</td>
+									<td>
+										<input type="text" className="form-control"/>
+									</td>
+								</tr>
+								<tr>
+									<td className="text-right">WIDTH:</td>
+									<td>
+										<input type="text" className="form-control"/>
+									</td>
+								</tr>
+								</tbody>
+						</table>
+					</div>
+				</div>
+				<hr/>
+				<div className="row text-center home-end">
+					<div className="col-xs-12">
+						<button className="btn btn-danger">GENERATE</button>
+					</div>
+				</div>
+			</div>
+		)
 		)
 	}
 }
