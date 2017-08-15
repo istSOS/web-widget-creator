7d3bcf13bd07b44a9200d22944f688a1615b2467
Luka <luka.glusica89@gmail.com>
2017-08-15 16:50:11 +0200

    Add config for istsos-widget lib

---
+++ webpack.widget.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+const ClosureCompilerPlugin = require('webpack-closure-compiler');
+module.exports = {
+   context: path.resolve(__dirname, './lib'),
+   entry: {
+      app: './index.js'
+   },
+   output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'istsos-widget.min.js',
+      library: 'istsos.widget',
+      libraryTarget: 'umd'
+   },
+   resolve: {
+      alias: {
+      	'WidgetTypes': path.resolve(__dirname, 'lib/WidgetTypes.js'),
+      	'WidgetFunctions': path.resolve(__dirname, 'lib/WidgetFunctions.js')
+      	'Widget': path.resolve(__dirname, 'lib/Widget.js')
+      }
+   },
+   module: {
+      rules: [
+         {
+            test: /\.js$/,
+            exclude: [/node_modules/],
+            use: [{
+               loader: 'babel-loader',
+               options: {
+                  presets: ['es2015']
+               },
+            }],
+         }
+      ],
+   },
+   plugins: [
+      new ClosureCompilerPlugin({
+         compiler: {
+            language_in: 'ECMASCRIPT6',
+            language_out: 'ECMASCRIPT5',
+            compilation_level: 'SIMPLE'
+         },
+         concurrency: 3,
+      })
+   ]
+};
