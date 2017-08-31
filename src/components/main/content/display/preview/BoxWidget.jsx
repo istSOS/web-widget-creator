4b4105bdee6856e4200bbfbdbb44249bbfa05442
Luka <luka.glusica89@gmail.com>
2017-08-31 10:59:57 +0200

    Update BoxWidget component

--- src/components/main/content/display/preview/BoxWidget.jsx
+++ src/components/main/content/display/preview/BoxWidget.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import moment from 'moment';
 
 class BoxWidget extends Component {
 	constructor(props) {
@@ -24,7 +25,7 @@ class BoxWidget extends Component {
 			   	<tbody>
 				      <tr>
 				        <td>{prop.display}:</td>
-				        <td>&nbsp;&nbsp;<i>{values[prop.name]} {prop.uom}</i></td>
+				        <td>&nbsp;&nbsp;<i>{parseFloat(values[prop.name]).toFixed(2)} {prop.uom}</i></td>
 				      </tr>
 			      </tbody>
 			   </table>
@@ -38,18 +39,15 @@ class BoxWidget extends Component {
 		console.log(this.props.preview)
 		let data = {
 			name: this.props.preview.procedure,
-			date: this.props.preview.dataConfig[this.props.preview.procedure].lastObservation.date.slice(0,10),
-			time: this.props.preview.dataConfig[this.props.preview.procedure].lastObservation.date.slice(11, 19),
-			gmt: this.props.preview.dataConfig[this.props.preview.procedure].lastObservation.date.slice(19,22),
+			date: this.props.preview.dataConfig[this.props.preview.procedure].lastObservation.date
 
 		}
 		return (
 			<div className="container text-center" style={{maxWidth: "600px"}}>
 			  <div className="row" style={{background: "green", color: "white", padding: "5px"}}>
-			    <div className="col-sm-3 text-center"><i><strong>{data.name}</strong></i></div>
-			    <div className="col-sm-3 text-center"><i>DATE: {data.date}</i></div>
-			    <div className="col-sm-3 text-center"><i>TIME: {data.time}</i></div>
-			    <div className="col-sm-3 text-center"><i>GMT: {data.gmt}</i></div>
+			    <div className="col-sm-4 text-center"><i><strong>{data.name}</strong></i></div>
+			    <div className="col-sm-4 text-center"><i>DATE: {moment(data.date).format('DD-MM-YYYY')}</i></div>
+			    <div className="col-sm-4 text-center"><i>TIME: {moment(data.date).format('HH:mm')}</i></div>
 			  </div>
 			  <div className="row">
 			  	 {this.generateItems(this.props.preview.dataConfig[this.props.preview.procedure].lastObservation.properties, this.props.preview.dataConfig[this.props.preview.procedure].observedSpec)} 
